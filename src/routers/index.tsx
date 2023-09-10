ee5b4b12de478176bf663a302b3a4a416a7d37ed
Alexey Rusel <alexeyrusel@gmail.com>
2023-09-10 21:39:44 +0300

    feat: add auth hoc for signIn/signUp pages

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -2,7 +2,7 @@ import { lazy } from "react";
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 
 import { AppRoutes } from "@/constants";
-import { RequireAuth } from "@/hocs";
+import { CheckAuth, RequireAuth } from "@/hocs";
 import { SuspenseLayout } from "@/layouts";
 
 const HomePage = lazy(() => import("@/pages/HomePage/homePage"));
@@ -16,10 +16,12 @@ const SignUpPage = lazy(() => import("@/pages/SignUpPage/signUpPage"));
 export const MainRouter = createBrowserRouter(
     createRoutesFromElements(
         <Route path={AppRoutes.HOME} errorElement={<ErrorPage />}>
-            <Route element={<SuspenseLayout />}>
-                <Route index element={<HomePage />} />
-                <Route path={AppRoutes.SIGN_IN} element={<SignInPage />} />
-                <Route path={AppRoutes.SIGN_UP} element={<SignUpPage />} />
+            <Route element={<CheckAuth />}>
+                <Route element={<SuspenseLayout />}>
+                    <Route index element={<HomePage />} />
+                    <Route path={AppRoutes.SIGN_IN} element={<SignInPage />} />
+                    <Route path={AppRoutes.SIGN_UP} element={<SignUpPage />} />
+                </Route>
             </Route>
             <Route element={<RequireAuth />}>
                 <Route path={AppRoutes.PROFILE} element={<ProfilePage />} />
